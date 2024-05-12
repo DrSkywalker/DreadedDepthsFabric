beed576e305c9b3283293b1641078036436c0108
DrSkywalker <efrye3@daltonstate.edu>
2024-05-11 21:12:17 -0400

    Initial commit

---
+++ src/main/java/lee/depths/Depths.java
@@ -0,0 +1,15 @@
+package lee.depths;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Depths implements ModInitializer {
+	public static final Logger LOGGER = LoggerFactory.getLogger("Dreaded Depths");
+	public static final String MODID = "depths";
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/lee/depths/DepthsDataGenerator.java
@@ -0,0 +1,11 @@
+package lee.depths;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class DepthsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
